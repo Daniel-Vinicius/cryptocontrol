78c27b04ab420535d72c2458d6fc73c2bffcafce
Daniel Vinícius <dev.dan.programador@gmail.com>
2021-10-24 19:12:50 -0300

    Config

---
+++ app.config.ts
@@ -0,0 +1,35 @@
+import { ExpoConfig, ConfigContext } from '@expo/config';
+
+export default ({ config }: ConfigContext): ExpoConfig => ({
+  ...config,
+  name: 'Crypto Control',
+  slug: 'cryptocontrol',
+  scheme: 'cryptocontrol',
+  version: '1.0.0',
+  orientation: 'portrait',
+  icon: './assets/icon.png',
+  description: 'Controle suas cryptos de forma muito simples!',
+  githubUrl: 'https://github.com/Daniel-Vinicius/cryptocontrol',
+  updates: {
+    enabled: true,
+  },
+  assetBundlePatterns: ['**/*'],
+  splash: {
+    image: './assets/splash.png',
+    resizeMode: 'contain',
+    backgroundColor: '#5636d3',
+  },
+  ios: {
+    supportsTablet: true,
+    bundleIdentifier: 'com.dan.cryptocontrol',
+    buildNumber: '1.0.0',
+  },
+  android: {
+    package: 'com.dan.cryptocontrol',
+    versionCode: 2,
+    adaptiveIcon: {
+      foregroundImage: './assets/adaptive-icon.png',
+      backgroundColor: '#5636d3',
+    },
+  },
+});
